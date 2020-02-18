db5cc331b3358da2ed7feaf4a0f22ebbf083a40a
marcel41 <mcube41xd@gmail.com>
2020-02-18 21:31:05 +0000

    startingToRead

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+  print ("GOOGLE HASH READING INPUT")
+  #uncomment the following two lines to open a file
+  #file1 = open(".txt", "r")
+  #file1.close()
+  #close file to avoid leak out
+
+def func1():
+    skip
+  #To be implemented
+
+def func2():
+    skip
+  #To be implemented
+
+if __name__== "__main__":
+  main()
